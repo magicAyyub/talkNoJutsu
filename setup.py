9c69591c82dd7c6e05f2c69b6003e3737bcb208f
Ayouba Doumbia <ayoubadoumbia041@gmail.com>
2024-07-29 23:15:40 +0200

    project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='noTalkJutsu',
+    version='1.0',
+    author='Ayouba Doumbia', 
+    author_email='ayoubadoumbia041@gmail.com',
+    packages=find_packages(),
+    long_description=open('README.md').read()
+)
